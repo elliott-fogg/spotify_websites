8b8b3877f8bd758aab9c0532b692d282eebcda67
Elliott Fogg <elliott.fogg@gmail.com>
2023-04-19 22:58:04 +0100

    Playlist query now outputs list of playlists on screen

--- custom_spotify_queries.js
+++ custom_spotify_queries.js
@@ -16,6 +16,17 @@ function fetchUserPlaylists() {
 	getUserPlaylists()
 	.then(data => {
 		console.log(data);
-		ulist.innerHTML = "Complete!";
+		ulist.innerHTML = "";
+		plist = data["items"];
+		for (let i = 0; i < plist.length; i++) {
+			let li = document.createElement("li");
+
+			let li_name = plist[i]["name"];
+			let li_len = plist[i]["tracks"]["total"];
+			let li_id = plist[i]["id"];
+
+			li.textContent = `${li_name} (${li_len}) - ${li_id}`;
+			ulist.appendChild(li);
+		}
 	})
 }
